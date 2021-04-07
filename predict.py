6b5ce3b90691fd75f143dcd8b117fc73a7afa7b4
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2021-04-07 17:34:57 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,9 +1,10 @@
 '''
 predict.py有几个注意点
-1、无法进行批量预测，如果想要批量预测，可以利用os.listdir()遍历文件夹，利用Image.open打开图片文件进行预测。
+1、该代码无法直接进行批量预测，如果想要批量预测，可以利用os.listdir()遍历文件夹，利用Image.open打开图片文件进行预测。
+具体流程可以参考get_miou_prediction.py，在get_miou_prediction.py即实现了遍历。
 2、如果想要保存，利用r_image.save("img.jpg")即可保存。
 3、如果想要原图和分割图不混合，可以把blend参数设置成False。
-4、如果想根据mask获取对应的区域，可以参考detect_image中，利用预测结果绘图的部分。
+4、如果想根据mask获取对应的区域，可以参考detect_image函数中，利用预测结果绘图的部分，判断每一个像素点的种类，然后根据种类获取对应的部分。
 seg_img = np.zeros((np.shape(pr)[0],np.shape(pr)[1],3))
 for c in range(self.num_classes):
     seg_img[:, :, 0] += ((pr == c)*( self.colors[c][0] )).astype('uint8')
