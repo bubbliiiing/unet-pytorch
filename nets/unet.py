34c97c576d28cc404dff2ac637af94d8e4521435
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2021-08-09 17:57:34 +0800

    Update unet.py

--- nets/unet.py
+++ nets/unet.py
@@ -9,13 +9,15 @@ class unetUp(nn.Module):
         self.conv1 = nn.Conv2d(in_size, out_size, kernel_size=3, padding=1)
         self.conv2 = nn.Conv2d(out_size, out_size, kernel_size=3, padding=1)
         self.up = nn.UpsamplingBilinear2d(scale_factor=2)
+        self.relu = nn.ReLU(inplace=True)
 
     def forward(self, inputs1, inputs2):
         outputs = torch.cat([inputs1, self.up(inputs2)], 1)
         outputs = self.conv1(outputs)
+        outputs = self.relu(outputs)
         outputs = self.conv2(outputs)
+        outputs = self.relu(outputs)
         return outputs
-
 
 class Unet(nn.Module):
     def __init__(self, num_classes=21, in_channels=3, pretrained=False):
